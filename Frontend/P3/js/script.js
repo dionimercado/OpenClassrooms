825a9d6fc3ad7c62d666a8dbbd8ae9bfa4965415
Dioni Mercado <me@dionimercado.com>
2018-03-25 20:52:01 -0400

    Footer improvements, schedule is now complete

--- Frontend/P3/js/script.js
+++ Frontend/P3/js/script.js
@@ -1,26 +1,13 @@
 jQuery( function($) {
+
+	// Smooth scrolling
 	jQuery('.navbar-nav>li>a').bind('click', function(event) {
 		var $anchor = jQuery(this);
 		jQuery('html, body').stop().animate({
-				scrollTop: (jQuery($anchor.attr('href')).offset().top - 0)
+				scrollTop: (jQuery($anchor.attr('href')).offset().top - 60)
 		}, 800, 'easeInOutExpo');
 		event.preventDefault();
 	});
 
 
 });
-
-class Stadium {
-	constructor(name, city, capacity) {
-		this.name = name;
-		this.city = city;
-		this.capacity = capacity;
-	}
- 
- describeStadium() {
-		console.log("the" +" "+ this.name + " " + "is in" +" "+ this.city + " " +"and holds"+" "+ this.capacity +" "+ "fans");
-	}
- }
- let Stadium1 = new Stadium("Mercedes Benz Arena", "Atlanta", 70000);
- 
- Stadium1.describeStadium();
